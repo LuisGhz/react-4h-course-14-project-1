313b523fb34e7e8b6effd9f662e46920ad74f0de
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-18 19:30:02 -0500

    Create text state and set it to input

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import './App.css';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [text, setText] = useState('');
+
   useEffect(() => {
     (async () => {
       const response = await axios.get('https://reqres.in/api/users');
       console.log(response)
       setUsers(response.data.data);
     })();
-  }, [])
+  }, []);
+
+  const onChangeHandler = text => {
+    setText(text);
+  }
 
   return (
     <div className="app">
-      <input type="text" />
+      <input type="text" 
+      onChange={ e => onChangeHandler(e.target.value) }
+      value={ text }
+      />
     </div>
   );
 }
