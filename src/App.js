b601abae2b925db2d5da5e5028ac6e5a68fe50a9
Luis Gutiérrez <luisghtz@outlook.com>
2021-06-18 19:24:49 -0500

    Get users from reqres with axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
   const [users, setUsers] = useState([]);
-  
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get('https://reqres.in/api/users');
+      console.log(response)
+      setUsers(response.data.data);
+    })();
+  }, [])
+
   return (
     <div>
       <input type="text" />
